21f562fdc423066624eca269f4d7be416be700be
Luka Flores <luka.flores@mail.com>
2022-08-19 12:19:17 -0700

    knn implementation tensorflow

--- MLKits/knn-tf/index.js
+++ MLKits/knn-tf/index.js
@@ -1,5 +1,60 @@
-require('@tensorflow/tfjs-node');
-const tf = require('@tensorflow/tfjs');
-const loadCSV = require('./load-csv');
+require("@tensorflow/tfjs-node-gpu");
+const tf = require("@tensorflow/tfjs");
+const loadCSV = require("./load-csv");
 
+function knn(features, labels, predictionPoint, k) {
+  const { mean, variance } = tf.moments(features, 0);
 
+  const scaledPrediction = predictionPoint.sub(mean).div(variance.pow(0.5));
+
+  return (
+    features
+      .sub(mean)
+      .div(variance.pow(2))
+      .sub(scaledPrediction)
+      .pow(2)
+      .sum(1)
+      .pow(0.5)
+      .expandDims(1)
+      .concat(labels, 1)
+      .unstack()
+      .sort((a, b) => (a.get(0) > b.get(0) ? 1 : -1))
+      .slice(0, k)
+      .reduce((acc, pair) => acc + pair.get(1), 0) / k
+  );
+}
+
+let { features, labels, testFeatures, testLabels } = loadCSV("kc_house_data.csv", {
+  shuffle: true,
+  splitTest: 10,
+  dataColumns: [
+    "bedrooms",
+    "bathrooms",
+    "sqft_living",
+    "sqft_lot",
+    "floors",
+    "waterfront",
+    "view",
+    "condition",
+    "grade",
+    "sqft_above",
+    "sqft_basement",
+    "yr_built",
+    "yr_renovated",
+    "zipcode",
+    "lat",
+    "long",
+    "sqft_living15",
+    "sqft_lot15",
+  ],
+  labelColumns: ["price"],
+});
+
+features = tf.tensor(features);
+labels = tf.tensor(labels);
+
+testFeatures.forEach((testPoint, i) => {
+  const results = knn(features, labels, tf.tensor(testPoint), 10);
+  const error = ((testLabels[i][0] - results) / testLabels[i][0]) * 100;
+  console.log("Guess", results, testLabels[0][0], `Error : ${error.toFixed(2)}%`);
+});
