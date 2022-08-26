682e121514b4d49bc482e9862bf3a123c74bca3f
Luka Flores <luka.flores@mail.com>
2022-08-26 09:12:38 -0700

    linear-regression start

--- MLKits/regressions/linear-regression/index.js
+++ MLKits/regressions/linear-regression/index.js
@@ -14,21 +14,5 @@ let { features, labels, testFeatures, testLabels } = loadCSV(
   }
 );
 
-const regression = new LinearRegression(features, labels, {
-  learningRate: 0.1,
-  iterations: 3,
-  batchSize: 10
-});
 
-regression.train();
-const r2 = regression.test(testFeatures, testLabels);
 
-plot({
-  x: regression.mseHistory.reverse(),
-  xLabel: 'Iteration #',
-  yLabel: 'Mean Squared Error'
-});
-
-console.log('R2 is', r2);
-
-regression.predict([[120, 2, 380]]).print();
