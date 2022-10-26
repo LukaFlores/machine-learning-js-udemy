9ec897bab2b4fb89b7870d8a50443d33eb53d049
Luka Flores <luka.flores@mail.com>
2022-10-26 09:56:55 -0700

    regression edits

--- MLKits/regressions/linear-regression/index.js
+++ MLKits/regressions/linear-regression/index.js
@@ -1,18 +1,23 @@
-require('@tensorflow/tfjs-node');
-const tf = require('@tensorflow/tfjs');
-const loadCSV = require('../load-csv');
-const LinearRegression = require('./linear-regression');
-const plot = require('node-remote-plot');
+require("@tensorflow/tfjs-node-gpu");
+const tf = require("@tensorflow/tfjs");
+const loadCSV = require("../load-csv.js");
+const LinearRegression = require("./linear-regression");
 
-let { features, labels, testFeatures, testLabels } = loadCSV(
-  '../data/cars.csv',
-  {
-    shuffle: true,
-    splitTest: 50,
-    dataColumns: ['horsepower', 'weight', 'displacement'],
-    labelColumns: ['mpg']
-  }
-);
+const path = require("path");
+const filePath = path.join(__dirname, "../data/cars.csv");
 
+let { features, labels, testFeatures, testLabels } = loadCSV(filePath, {
+  shuffle: true,
+  splitTest: 50,
+  dataColumns: ["horsepower"],
+  labelColumns: ["mpg"],
+});
 
+const regression = new LinearRegression(features, labels, {
+  learningRate: 0.01,
+  iterations: 100,
+});
 
+regression.train();
+
+console.log("M:", regression.m, "B:", regression.b);
